0ab24959be38b17650aea3ded9cdedbe11badd5e
mollymoog <mmkover@gmail.com>
2020-09-11 13:38:39 -0400

    added plant_names method

--- lib/api.rb
+++ lib/api.rb
@@ -5,19 +5,20 @@ require 'pry'
 require_relative 'plants'
 
 class API
+  attr_accessor :plants_names
 
   def self.scrape_plants
     resp = RestClient.get('https://trefle.io/api/v1/plants?token=28lRdBdWEwCfHbsosT8M3JBKY6gaVRvzEzUyBvBEz7Q')
     plants_hash = JSON.parse(resp.body, symbolize_names:true)
     plants_arr = plants_hash[:data]
 
-    plants_names = plants_arr.collect do | plant |
+    plants_arr.collect do | plant |
       Plants.new(plant)
     end
   end
 
-  def self.scrap_plant_details (plant)
-    plants_names.each do | plant |
+  def self.scrape_plant_details (plant)
+    # @plants_names.each do | plant |
       resp = RestClient.get('https://trefle.io' + plant.url + '?token=28lRdBdWEwCfHbsosT8M3JBKY6gaVRvzEzUyBvBEz7Q')
       plant_hash = JSON.parse(resp.body, symbolize_names:true)
 
@@ -30,7 +31,7 @@ class API
       plant.lang_es = plant_hash[:data][:main_species][:common_names][:es]
       plant.lang_fr = plant_hash[:data][:main_species][:common_names][:fr]
       plant.lang_en = plant_hash[:data][:main_species][:common_names][:en]
-    end
+    # end
   end
 
 end
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,27 +1,26 @@
 class CLI
 
+
+
     def run
-        puts "Welcome to Fern's Friends, lets find some friends"
-     
+        puts "Welcome to Fern's Friends, lets meet some other plants"
         API.scrape_plants
-        list_plants
         menu
     end
 
-    def list_plants
-        Plants.all.each.with_index(1) do |plant, i|
-            puts "#{i}. #{plant.name}"
-        end
-    end
-
     def menu
-        puts "pick a number to get to learn more"
+        list_plants
+        puts "pick a number to learn more"
         input = gets.chomp
 
         if 0 < input.to_i && input.to_i <= 20
             plant = Plants.all[input.to_i - 1]
+            load_plant(plant)
 
-            puts "You've selected #{plant.name}"
+            puts "You've selected #{plant.name}, more scientifically referred to as '#{plant.scientific_name}'.'"
+            puts "What else would you like to know?"
+            puts ""
+            plant_names(plant)
         else
             puts "That number isn't on this page."
             puts "If you'd like to see the next page, type 'next'"
@@ -32,17 +31,32 @@ class CLI
                 else
                     menu
                 end
-                       
         end 
     end
 
+    def list_plants
+        Plants.all.each.with_index(1) do |plant, i|
+            puts "#{i}. #{plant.name}"
+        end
+    end
 
+    def plant_names (plant)
+        puts "what language would you like to translate to :"
+        puts "type 'spanish', 'french', or 'english' to see the common names in that language"
+        lang_input = gets.chomp.downcase
 
-    def plant_names
-        puts "what language would you like to use"
-        puts "type "
+        case lang_input
+        when "spanish"
+            plant.lang_es.each.with_index(1) { |name, i| puts "#{i}. #{name}"}
+        when "french"
+            plant.lang_fr.each.with_index(1) { |name, i| puts "#{i}. #{name}"}
+        when "english"
+            plant.lang_en.each.with_index(1) { |name, i| puts "#{i}. #{name}"}
+        end    
+    end
 
-
+    def load_plant (plant)
+        API.scrape_plant_details(plant)
     end
 
 end
